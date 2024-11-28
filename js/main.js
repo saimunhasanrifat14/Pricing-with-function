daa042bb45be21c24f4510fe133c9b063293ef72
Saimun Hasan <saimunhasanrifat14@gmail.com>
2024-11-28 18:24:40 +0600

    finish js code

--- js/main.js
+++ js/main.js
@@ -11,30 +11,22 @@ const takeAcualPrice = +prompt('Enter your buing price ');
 // vatAdder Function implement
 const vatAdder = (actualPrice = 0, vat = 10)=> {
     const vatWithPrice = actualPrice + (actualPrice*vat/100);
-    const vatDetails = [vatWithPrice , vat];
-    return vatDetails;
+    return [vatWithPrice , vat]
     
 }
 
 // make discount function imlement
-const discountMaker = (vatWithPrice =0 , discountRate = 30) => {
+const discountMaker = (vatWithPrice = 0 , discountRate = 30) => {
     const discountPrice = vatWithPrice - (vatWithPrice*discountRate/100)
-    const discountDetails = [discountPrice , discountRate]
-    return discountDetails;
+    return [discountPrice , discountRate]
 }
 
-// make a display function 
-const display = (PactualPrice , pVatWihtPrice , pVat , pDiscountPrice, pDiscountrate) => {
-console.log(`
-    -----------------Invoice-------------------
-    Actual Price    : ${PactualPrice}
-    Vat             : ${pVat}
-    vat Price       : ${pVatWihtPrice}
-    discount rate   : ${pDiscountrate}
-    discount price  : ${pDiscountPrice}
-    `);
-
-}
 const RecivedVatDetails = vatAdder(takeAcualPrice , 10)
-const recivedDiscountDetails = discountMaker(recivedDiscountDetails[0], 30);
-display(takeAcualPrice , RecivedVatDetails[0], RecivedVatDetails[1], recivedDiscountDetails[0], recivedDiscountDetails[1])
+const recivedDiscountDetails = discountMaker( RecivedVatDetails[0] , 30);
+
+// Display the result
+document.getElementById("actualPrice").innerHTML = takeAcualPrice;
+document.getElementById("ProductVat").innerHTML = `${RecivedVatDetails[1]}%`;
+document.getElementById("ProductPriceWithVat").innerHTML = `${RecivedVatDetails[0]}`;
+document.getElementById("ProductDiscout").innerHTML = `${recivedDiscountDetails[1]}%`;
+document.getElementById("FinalProductPrice").innerHTML = `${recivedDiscountDetails[0]}`;
