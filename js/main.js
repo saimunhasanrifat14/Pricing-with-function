ed2490692f5cada393ada0813b2a84ee380d401f
Saimun Hasan <saimunhasanrifat14@gmail.com>
2024-11-28 15:08:30 +0600

    added more two function

--- js/main.js
+++ js/main.js
@@ -8,21 +8,33 @@
 
 const takeAcualPrice = +prompt('Enter your buing price ');
 
-
+// vatAdder Function implement
 const vatAdder = (actualPrice = 0, vat = 10)=> {
-    const vatFurmula = actualPrice + (actualPrice*vat/100);
+    const vatWithPrice = actualPrice + (actualPrice*vat/100);
     const vatDetails = [vatWithPrice , vat];
-    return vatDetails
+    return vatDetails;
     
 }
 
-
+// make discount function imlement
 const discountMaker = (vatWithPrice =0 , discountRate = 30) => {
-    const discountPrice = 100 - (100*10/100)
+    const discountPrice = vatWithPrice - (vatWithPrice*discountRate/100)
     const discountDetails = [discountPrice , discountRate]
-    return discountDetails
+    return discountDetails;
 }
 
+// make a display function 
+const display = (PactualPrice , pVatWihtPrice , pVat , pDiscountPrice, pDiscountrate) => {
+console.log(`
+    -----------------Invoice-------------------
+    Actual Price    : ${PactualPrice}
+    Vat             : ${pVat}
+    vat Price       : ${pVatWihtPrice}
+    discount rate   : ${pDiscountrate}
+    discount price  : ${pDiscountPrice}
+    `);
 
-const RecivedVatDetails = vatAdder(400 , 10)
-console.log(RecivedVatDetails);
+}
+const RecivedVatDetails = vatAdder(takeAcualPrice , 10)
+const recivedDiscountDetails = discountMaker(recivedDiscountDetails[0], 30);
+display(takeAcualPrice , RecivedVatDetails[0], RecivedVatDetails[1], recivedDiscountDetails[0], recivedDiscountDetails[1])
